04b91af220067a2e032bd923e0fe7c7694706a49
Saif Rashed <saifeddinerashed@icloud.com>
2026-02-07 14:46:34 +0100

    fix: edited config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://saifrashed.github.io',
+	site: 'https://saifrashed.com',
 
 	integrations: [
 		starlight({
