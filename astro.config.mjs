5d60a59050721bb735b622632a09b166d616e681
Saif Rashed <saifeddinerashed@icloud.com>
2026-02-08 12:05:47 +0100

    fix: removed extra cname record and added correct site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://saifrashed.com',
+	site: 'https://www.saifrashed.com',
 
 	integrations: [
 		starlight({
